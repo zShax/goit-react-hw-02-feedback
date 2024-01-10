07fd0367d05ac2d523b92f5796c86b4376d7695d
zShax <71908965+zShax@users.noreply.github.com>
2024-01-10 17:10:15 +0200

    fixed package.json and removed obsolete styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-// Styles
-// import styles from './App.module.css';
-
 // Feedback Form
 import FeedbackForm from './feedbackForm/FeedbackForm';
-
 // Section
 import Section from './section/Section';
 
